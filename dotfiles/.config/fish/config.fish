624d8defd8989e447df06361750933bdc0b93a8e
Elias <supgesu@gmail.com>
2022-01-07 15:44:47 +0100

    Update config.fish

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -2,10 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Disable welcome message
 set -U fish_greeting
-
-set PATH $PATH /home/$USER/bin
 
 # Page up and down keys search through the history
 bind -k ppage history-search-backward
 bind -k npage history-search-forward
+
+set PATH $PATH /home/$USER/bin
