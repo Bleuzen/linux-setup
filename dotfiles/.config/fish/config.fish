dde9a05c86dd20be06f7ed32aa2267f2ba0dfe05
Ilijaz Mehmedovic <35637072+IlijazM@users.noreply.github.com>
2022-01-07 15:43:45 +0100

    add page up and down keys search through history (#1)

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -5,3 +5,7 @@ end
 set -U fish_greeting
 
 set PATH $PATH /home/$USER/bin
+
+# Page up and down keys search through the history
+bind -k ppage history-search-backward
+bind -k npage history-search-forward
