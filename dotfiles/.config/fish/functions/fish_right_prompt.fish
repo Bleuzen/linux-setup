6e372f0e3ae99623d65fdbbe3f6a098f51c92429
Elias <supgesu@gmail.com>
2022-01-07 15:45:50 +0100

    Update fish_right_prompt.fish

--- dotfiles/.config/fish/functions/fish_right_prompt.fish
+++ dotfiles/.config/fish/functions/fish_right_prompt.fish
@@ -1,7 +1,7 @@
 function fish_right_prompt
     set -l cmd_status $status
     if test $cmd_status -ne 0
-        echo -n (set_color red)"[$cmd_status] "
+        echo -n (set_color brgrey)"[$cmd_status] "
     end
 
     set -l duration "$CMD_DURATION"
