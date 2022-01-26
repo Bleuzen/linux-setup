c9964c67ab5f2d2293f00a4571f26ae69884f21d
Elias <supgesu@gmail.com>
2022-01-26 20:28:34 +0100

    Delete mark-kernels-autoinstalled.sh

--- os/kubuntu/mark-kernels-autoinstalled.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $EUID -ne 0 ]];
-then
-    exec sudo /bin/bash "$0" "$@"
-fi
-
-apt-mark auto $(apt-mark showmanual | grep -E "^linux-([[:alpha:]]+-)+[[:digit:].]+-[^-]+(|-.+)$")
