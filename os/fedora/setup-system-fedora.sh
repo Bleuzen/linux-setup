9b1158fa957fa3984541e8a5b40e35ff98c36059
Bleuzen <12885163+Bleuzen@users.noreply.github.com>
2022-11-26 23:06:53 +0100

    fedora: install additional codecs

--- os/fedora/setup-system-fedora.sh
+++ os/fedora/setup-system-fedora.sh
@@ -21,15 +21,47 @@ function update_system {
     dnf update --refresh -y
 }
 
-function install_nvidia_driver {
+function enable_nvidia_filtered_rpmfusion {
     dnf install fedora-workstation-repositories
     dnf config-manager --set-enable rpmfusion-nonfree-nvidia-driver
-    dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
 }
 
 function enable_rpmfusion {
     dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     dnf groupupdate -y core
+}
+
+function install_nvidia_driver {
+    dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
+}
+
+function rpmfusion_install_additional_codecs {
+    # Source: https://rpmfusion.org/Howto/Multimedia
+    dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+    dnf groupupdate sound-and-video -y
+}
+
+function rpmfusion_enable_hardware_codecs_intel_recent {
+    dnf install -y intel-media-driver
+}
+
+function rpmfusion_enable_hardware_codecs_intel_older {
+    dnf install -y libva-intel-driver
+}
+
+function rpmfusion_enable_hardware_codecs_amd {
+    dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
+    dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+}
+
+function rpmfusion_enable_hardware_codecs_amd_i686 {
+    # If using i686 compat libraries (for steam or alikes)
+    dnf swap -y mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
+    dnf swap -y mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
+}
+
+function rpmfusion_enable_hardware_codecs_nvidia {
+    dnf install -y nvidia-vaapi-driver
 }
 
 function setup_flathub {
@@ -47,7 +79,15 @@ dnf_config_defaultyes
 dnf_config_disable_install_weak_deps
 packages_cleanup
 update_system
+# enable_nvidia_filtered_rpmfusion
+enable_rpmfusion
 # install_nvidia_driver
-# enable_rpmfusion
+rpmfusion_install_additional_codecs
+# rpmfusion_enable_hardware_codecs_intel_recent
+# rpmfusion_enable_hardware_codecs_intel_older
+# rpmfusion_enable_hardware_codecs_amd
+# rpmfusion_enable_hardware_codecs_amd_i686
+# rpmfusion_enable_hardware_codecs_nvidia
 setup_flathub
 # allow_flatpak_read_gtk3_theme
+
